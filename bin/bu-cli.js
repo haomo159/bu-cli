05d7d75a876045da1a2032100eaed75b3fa41873
martinlee <martinlee159@163.com>
2019-06-17 17:52:46 +0800

    Add atp60 publish

--- bin/bu-cli.js
+++ bin/bu-cli.js
@@ -2,16 +2,38 @@
 
 'use strict'
 
-const program = require('commander')
+const commander = require('commander')
+const program = new commander.Command()
 
 const pkg = require('../package')
+const createcontract = require('../lib/createContract')
 
 const VERSION = pkg.version
 const DESCRIPTION = pkg.description
 
 program
+  .name('bu-atp60')
   .version(VERSION)
   .description(DESCRIPTION)
   .usage('[command] [options]')
-  .command('atp60', 'publish ATP60 token or import SKU information')
+  .option('-p, --publish', 'publish atp60')
+  .option('-k, --key <key>', 'private key')
+  .option('-H, --host <host>', 'contract address')
   .parse(process.argv)
+
+// 发布合约
+if (program.publish) {
+  if (!program.key) {
+    console.log(`option '-k, --key <key>' argument missing`)
+  }
+  if (!program.host) {
+    console.log(`option '-h, --host <host>' argument missing`)
+  }
+  createcontract(program.host, program.key)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
